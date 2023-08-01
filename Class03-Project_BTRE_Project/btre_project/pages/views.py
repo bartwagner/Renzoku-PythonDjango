f2350810dcb4e42d5ecbbbce5c1fecd8d94a552f
bartwagner <118787799+bartwagner@users.noreply.github.com>
2023-08-01 14:17:28 -0700

    listing of houses

--- Class03-Project_BTRE_Project/btre_project/pages/views.py
+++ Class03-Project_BTRE_Project/btre_project/pages/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from listings.models import Listing
+from listings.choices import price_choices, bedroom_choices, state_choices
 from realtors.models import Realtor
+
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     context = {
-        'listings': listings
+        'listings': listings,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices, 
+        'state_choices' : state_choices
     }
     return render(request, 'pages/index.html', context)
 
